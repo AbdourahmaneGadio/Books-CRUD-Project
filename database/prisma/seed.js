77fdae56f1ce1212c8a6a4bae755a01fa0294ec3
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2024-12-12 14:38:56 +0100

    Modif du seed pour les donnees de base + fichier .env

--- database/prisma/seed.js
+++ database/prisma/seed.js
@@ -1,36 +1,44 @@
-const { PrismaClient } = require('@prisma/client');
+const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-    const authors = [
-        { name: 'J.K. Rowling' },
-        { name: 'George R.R. Martin' },
-        { name: 'Stephen King' },
-        { name: 'Agatha Christie' },
-    ];
 
-    for (const author of authors) {
-        await prisma.authors.create({
-            data: author,
-        });
+    const dbAuthors = await prisma.authors.count();
+    const dbCategories = await prisma.categories.count();
+
+    if (dbAuthors === 0 && dbCategories === 0) {
+        const authors = [
+            {name: 'J.K. Rowling'},
+            {name: 'George R.R. Martin'},
+            {name: 'Stephen King'},
+            {name: 'Agatha Christie'},
+        ];
+
+        for (const author of authors) {
+            await prisma.authors.create({
+                data: author,
+            });
+        }
+
+        // Seed categories
+        const categories = [
+            {name: 'Fiction'},
+            {name: 'Non-Fiction'},
+            {name: 'Science Fiction'},
+            {name: 'Mystery'},
+            {name: 'Fantasy'},
+        ];
+
+        for (const category of categories) {
+            await prisma.categories.create({
+                data: category,
+            });
+        }
+
+        console.log('Seed data inserted successfully.');
+    } else {
+        console.log('Database structure already exists. Skipping the seeding...');
     }
-
-    // Seed categories
-    const categories = [
-        { name: 'Fiction' },
-        { name: 'Non-Fiction' },
-        { name: 'Science Fiction' },
-        { name: 'Mystery' },
-        { name: 'Fantasy' },
-    ];
-
-    for (const category of categories) {
-        await prisma.categories.create({
-            data: category,
-        });
-    }
-
-    console.log('Seed data inserted successfully.');
 }
 
 main()
