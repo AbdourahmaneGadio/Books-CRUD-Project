4a8520467c3acf4535142823ee6e63b2592296fc
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2024-12-12 17:48:28 +0100

    Remplacement des valeurs pour remplacer la table games par Books

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
+// Source : https://github.com/nailtonvital/react-node-mysql-crud/blob/master/server/index.js
+
 const express = require('express');
 const server = express();
 const mysql = require('mysql');
 const cors = require('cors');
 
 const db = mysql.createPool({
-    host:  process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "nailton123",
     database: process.env.DB_NAME || "crudgames",
@@ -13,56 +15,76 @@ const db = mysql.createPool({
 server.use(express.json());
 server.use(cors());
 
-server.post("/register", (req, res) => {
-    const { name } = req.body;
-    const { cost } = req.body;
-    const { category } = req.body;
+server.post("/books/create", (req, res) => {
+    const {title} = req.body;
+    const {synopsis} = req.body;
+    const {authorId} = req.body;
+    const {price} = req.body;
+    const {isbn} = req.body;
+    const {pageNum} = req.body;
+    const {categoryId} = req.body;
 
-    let sql = "INSERT INTO games (name, cost, category) VALUES (?,?,?)"
-    db.query(sql, [name, cost, category], (err,result) =>{
+    let sql = "INSERT INTO Books (title, synopsis, authorId, price, isbn, pageNum, categoryId) VALUES (?,?,?,?,?,?,?)"
+    db.query(sql, [title, synopsis, authorId, price, isbn, pageNum, categoryId], (err, result) => {
         if (err) {
             console.log(err);
-        }else{
+            res.status(500).send(err);
+        } else {
             console.log(result);
+            res.status(200).send(result);
         }
     })
 });
 
-server.get("/games", (req, res) => {
+server.get("/books", (req, res) => {
 
-    let sql = "SELECT * FROM games";
-    db.query(sql, (err,result) =>{
+    let sql = "SELECT * FROM Books";
+    db.query(sql, (err, result) => {
         if (err) {
             console.log(err);
-        }else{
-            res.send(result);
+            res.status(500).send(err);
+        } else {
+            console.log(result);
+            res.status(200).send(result);
         }
-
     })
 });
 
-server.put("/edit", (req, res) => {
-    const { id } = req.body;
-    const { name } = req.body;
-    const { cost } = req.body;
-    const { category } = req.body;
+server.put("/books/edit", (req, res) => {
+    const {id} = req.body;
+    const {title} = req.body;
+    const {synopsis} = req.body;
+    const {authorId} = req.body;
+    const {price} = req.body;
+    const {isbn} = req.body;
+    const {pageNum} = req.body;
+    const {categoryId} = req.body;
 
-    let sql = "UPDATE games SET name = ?, cost = ?, category = ? WHERE idgames = ?";
-    db.query(sql, [name, cost, category, id], (err,result) =>{
+    let sql = "UPDATE Books SET title = ?, synopsis = ?, authorId = ?, price = ?, isbn = ?, pageNum = ?, categoryId = ? WHERE id = ?";
+    db.query(sql, [title, synopsis, authorId, price, isbn, pageNum, categoryId, id], (err, result) => {
         if (err) {
             console.log(err);
-        }else{
-
-            res.send(result);
+            res.status(500).send(err);
+        } else {
+            console.log(result);
+            res.status(200).send(result);
         }
     })
 });
 
-server.delete("/delete/:index", (req,res) =>{
-    const { index } = req.params
+server.delete("/books/delete/:index", (req, res) => {
+    const {index} = req.params
 
-    let sql = "DELETE FROM games WHERE idgames = ?"
-    db.query(sql, [index], (err,result) =>{err ? console.log(err) : res.send(result)})
+    let sql = "DELETE FROM Books WHERE id = ?"
+    db.query(sql, [index], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            console.log(result);
+            res.status(200).send(result);
+        }
+    })
 })
 server.listen(3001, () =>
     console.log("Running in the port 3001")
